d80392ca2481f7b38be57f8a98f4d41e8d828076
Chris West (Faux) <git@goeswhere.com>
2014-08-18 20:42:52 +0100

    ExpEvolution test

---
+++ src/main/java/com/goeswhere/jmh/ExpEvolution.java
@@ -0,0 +1,40 @@
+package com.goeswhere.jmh;
+
+import org.apache.commons.math3.util.FastMath;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+@State(Scope.Thread)
+public class ExpEvolution {
+
+    double value = 0.123456789d;
+
+    @Benchmark
+    public double original() {
+        return 1.0 / (1 + Math.exp(-1.5 * Math.pow(value, 2) + 2));
+    }
+
+    @Benchmark
+    public double cleanJava() {
+        return 1.0 / (1 + Math.exp(-1.5 * value * value + 2));
+    }
+
+    @Benchmark
+    public double fastMath() {
+        return 1.0 / (1 + FastMath.exp(-1.5 * value * value + 2));
+    }
+
+    @Benchmark
+    public double approximation() {
+        final double sqr = value * value;
+        if (sqr >= 0 && sqr < 1) {
+            // import math; import numpy as np;
+            // g=lambda a:1/(1+math.exp(-1.5*a+2));
+            // x = np.arange(0,1,0.0001); y = [g(a) for a in x];
+            // c=np.polyfit(x, y, 2).tolist(); c
+            return 0.1034274489106936 * sqr * sqr + 0.15550046971524753 * sqr + 0.11920662709562624;
+        }
+        return 1.0 / (1 + FastMath.exp(-1.5 * value * value + 2));
+    }
+}
---
+++ src/test/java/com/goeswhere/jmh/ExpEvolutionTest.java
@@ -0,0 +1,20 @@
+package com.goeswhere.jmh;
+
+public class ExpEvolutionTest {
+    public static void main(String[] args) {
+        final ExpEvolution ut = new ExpEvolution();
+        double diff = 0;
+        long count = 0;
+        for (double i = 0; i < 1; i += 0.00001) {
+            ++count;
+            ut.value = i;
+            diff += square(ut.fastMath() - ut.approximation());
+        }
+
+        System.out.println(diff / count);
+    }
+
+    private static double square(double v) {
+        return v*v;
+    }
+}
